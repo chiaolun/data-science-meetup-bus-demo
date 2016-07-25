11d5e48ecef39e74e65e72bef3620132adf85c9f
Chiao Cheng <chiao@alum.mit.edu>
2016-07-25 04:11:53 -0400

    added comments

--- 0-scrape.py
+++ 0-scrape.py
@@ -6,14 +6,23 @@ url = "http://pda.5284.com.tw/MQS/businfo2.jsp?routeId={route:d}"
 
 
 def get_html(route):
+    # request page
     res = requests.get(url.format(route=route))
+    # check that request was successful - if not, return None - may
+    # want to consider throwing an exception here
     if res.status_code != 200:
         return None
+    # parse html
     soup = bs4.BeautifulSoup(res.text, "lxml")
+    # replace <br> elements with actual spaces
     for br in soup.find_all("br"):
         br.replace_with(" ")
+    # get all the tables
     tables = soup.find_all("table")
 
+    # extract all rows from tables 3 and 4, return it as two vectors
+    # (for the two directions) each with many (location_name, state)
+    # pairs
     return [[[x.text for x in r.find_all("td")]
             for r in t.find_all("tr")]
             for t in tables[3:5]]
@@ -21,10 +30,17 @@ def get_html(route):
 if __name__ == "__main__":
     import json
     import time
+    # infinite loop!
     while 1:
+        # put everything in a try so that we can continue even if
+        # things go wrong
         try:
+            # get the data for this point in time
             data = get_html(650)
+            # make sure request was successful
             if data is not None:
+                # save data as json to data folder, putting the route
+                # and time in the filename
                 json.dump(
                     data,
                     open("data/{}_{:.0f}.json".format(650, time.time()), "w")
@@ -32,4 +48,5 @@ if __name__ == "__main__":
             print(time.time(), data)
         except Exception as e:
             print(e)
+        # wait 30 seconds before fetching again
         time.sleep(30)
--- 1-analyze.py
+++ 1-analyze.py
@@ -6,46 +6,59 @@ import numpy as np
 import glob
 import re
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.set_option("display.width", 200)
 
-files = glob.glob("data/*.json")
-
 
 def parse_file(file0):
+    "reads json into a pandas datafram"
+    # parses file name to get the route and the time
     bus0, time0 = re.match('.*/(\d+)_(\d+)\.json', file0).groups()
     bus0 = int(bus0)
     time0 = (
         pd.to_datetime(time0, unit='s') +
         pd.Timedelta(hours=8)
     )
+    # opens json file - note that I use ujson here because it is
+    # faster
     data0 = ujson.load(open(file0))
+    # list comprehension to get a flat vector from the json
+    df = [(bus0, time0, i, j, location0, state0)
+          for i, t0 in enumerate(data0)
+          for j, (location0, state0) in enumerate(t0)]
+    # convert flat vector into a dataframe
     df = pd.DataFrame(
-        [(bus0, time0, i, j, location0, state0)
-         for i, t0 in enumerate(data0)
-         for j, (location0, state0) in enumerate(t0)],
-        columns=["bus", "time", "direction", "loc_no",
-                 "loc", "state"]
+        df,
+        columns=["bus", "time", "direction", "loc_no", "loc", "state"]
     )
     return df
 
+# list all files
+files = glob.glob("data/*.json")
+# parse all files and concat the individual dataframes together
 dat = pd.concat([parse_file(file0) for file0 in sorted(files)])
 
 print(dat.head(30))
 
+# construct regex for parsing out the vehicle identifiers
 vehicle_chain_re = ["( \d{3}-\w{2})?"] * 5
 vehicle_chain_re = "(\d{3}-\w{2})" + "".join(vehicle_chain_re)
 print(vehicle_chain_re)
 
 traj_idx = ["time", "direction", "loc_no"]
 
+# extract vehicle numbers at the beginning of strings
 vehicle = dat.state.str.extract("^" + vehicle_chain_re, expand=True)
 vehicle = pd.concat([dat[traj_idx], vehicle], axis=1)
 vehicle = vehicle.set_index(traj_idx)
 print(vehicle.head(50))
 
+# extract vehicle numbers at the end of strings
 vehicle_post = dat.state.str.extract(vehicle_chain_re + "$", expand=True)
 vehicle_post = pd.concat([dat[traj_idx], vehicle_post], axis=1)
+# additional two steps to shift the label forward by one, since label
+# at the end of the string means the bus has passed that station
 vehicle_post = vehicle_post.set_index(traj_idx)
 vehicle_post = vehicle_post.groupby(level=["time", "direction"]).shift()
 print(vehicle_post.head(50))
@@ -68,6 +81,11 @@ print(traj.head(10))
 
 gap = traj.groupby(["vehicle", "direction"]).time.diff()
 
+# due to pandas' lack of a non-sorted groupby, we use the
+# unequal + cumsum trick to get groupings, we we call trips
+
+# we also start a new trip whenever there is a gap greater than one
+# minute
 traj["trip"] = (
     ((((traj[["vehicle", "direction"]] !=
         traj[["vehicle", "direction"]].shift())
@@ -75,15 +93,27 @@ traj["trip"] = (
       (gap > pd.Timedelta(minutes=1)))
      .cumsum())
 )
+# only keep trips with more than 10 data points
 trip_pts = traj.groupby("trip").time.count()
 traj.merge(trip_pts[trip_pts > 10].reset_index()[["trip"]])
 traj = traj.merge(trip_pts[trip_pts > 10].reset_index()[["trip"]])
-print(traj.head(10))
+trip_pts = trip_pts.sort_values()
+print(trip_pts.tail(10))
 
-print(traj[traj.trip == 7].loc[50:58].merge(dat))
+# Plot some trips
+(traj[traj.trip.isin(trip_pts.tail(10).index)]
+ .set_index(["time", "direction", "trip"])
+ .unstack([-1, -2]).loc_no.plot())
+plt.show()
 
-traj0 = traj[traj.trip == 7].loc[50:58][["time", "direction"]].merge(dat)
-print(traj0[traj0.loc_no.isin(range(26, 29))]
+
+# investigate anomalies
+traj[traj.trip == 209].loc_no.plot()
+plt.show()
+print(traj[traj.trip == 209])
+
+traj0 = traj[traj.trip == 209].loc[3640:3650][["time", "direction"]].merge(dat)
+print(traj0[traj0.loc_no.isin(range(30, 40))]
       .set_index(["time", "loc_no"])
       .unstack().state)
 
